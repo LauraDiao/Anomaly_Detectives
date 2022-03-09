e0f00c27a753f62106b5b0d946949271c0aebf5c
Ben243 <49301702+Ben243@users.noreply.github.com>
2022-03-08 22:09:33 -0800

    run.py work pls

--- src/make_dir.py
+++ src/make_dir.py
@@ -6,10 +6,14 @@ warnings.filterwarnings("ignore")
 def init_():
     """creates directories needed to run repo"""
 
+    data_path = "data"
     raw_path = "data/raw"
     raw_c_path = "data/raw/train_c"
     raw_r_path = "data/raw/train_r"
 
+    if not os.path.isdir(data_path):
+        os.mkdir(data_path)
+        
     if not os.path.isdir(raw_path):
         os.mkdir(raw_path)
         os.mkdir(raw_c_path)
